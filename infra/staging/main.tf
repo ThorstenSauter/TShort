4c7b0303cec3360ab895e8626c5a95df57df02be
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2025-01-02 23:49:57 +0100

    fix: Terraform formatting

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -6,11 +6,11 @@ data "cloudflare_zone" "main" {
 
 locals {
   app_name            = "tshort"
-  domain_splits = split(".", var.primary_domain)
+  domain_splits       = split(".", var.primary_domain)
   domain_split_length = length(local.domain_splits)
   tld                 = local.domain_splits[local.domain_split_length - 1]
-  root_domain = join(".", [local.domain_splits[local.domain_split_length - 2], local.tld])
-  subdomain = coalesce(join(".", slice(local.domain_splits, 0, local.domain_split_length - 2)), var.primary_domain)
+  root_domain         = join(".", [local.domain_splits[local.domain_split_length - 2], local.tld])
+  subdomain           = coalesce(join(".", slice(local.domain_splits, 0, local.domain_split_length - 2)), var.primary_domain)
   resource_group_name = "rg-${local.app_name}-${var.env}-${var.location}-${var.resource_id}"
   static_site_name    = "stapp-${local.app_name}-${var.env}-${var.resource_id}"
 }
@@ -39,7 +39,7 @@ resource "cloudflare_record" "primary" {
 }
 
 resource "time_sleep" "wait_for_dns_record" {
-  depends_on = [cloudflare_record.primary]
+  depends_on      = [cloudflare_record.primary]
   create_duration = "2m"
 }
 
@@ -47,5 +47,5 @@ resource "azurerm_static_web_app_custom_domain" "primary" {
   static_web_app_id = azurerm_static_web_app.main.id
   domain_name       = var.primary_domain
   validation_type   = "cname-delegation"
-  depends_on = [time_sleep.wait_for_dns_record]
+  depends_on        = [time_sleep.wait_for_dns_record]
 }

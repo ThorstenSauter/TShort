6fb6925c38dd67d72b6532efa2ca948fbadbfd06
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2025-01-03 18:25:03 +0100

    fix: added workload consumption profile

--- infra/production/api.tf
+++ infra/production/api.tf
@@ -66,6 +66,7 @@ resource "azurerm_container_app" "api" {
   container_app_environment_id = azurerm_container_app_environment.main.id
   resource_group_name          = azurerm_resource_group.main.name
   revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
   identity {
     type = "UserAssigned"
     identity_ids = [

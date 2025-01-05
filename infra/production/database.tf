1b27df6c0adee66cf1942987dab6396c7a9e2355
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2025-01-05 06:01:28 +0100

    fix(infra): production database name and firewall rule (#24)

--- infra/production/database.tf
+++ infra/production/database.tf
@@ -40,9 +40,16 @@ resource "azurerm_mssql_server" "main" {
   tags = var.tags
 }
 
+resource "azurerm_mssql_firewall_rule" "main" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.main.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 resource "azapi_resource" "sql_database" {
-  type      = "Microsoft.Sql/servers/databases@2023-08-01-preview"
-  name      = "sqldb-001"
+  type      = "Microsoft.Sql/servers/databases@2024-05-01-preview"
+  name      = local.database_name
   location  = azurerm_resource_group.main.location
   parent_id = azurerm_mssql_server.main.id
 

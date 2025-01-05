d1ff7329d1c5fcdd1e3b05f42ca98d1a4e6927b6
Thorsten Sauter <Thorsten.Sauter@gmail.com>
2025-01-05 05:02:40 +0100

    feat: add Entity Framework Core migration bundle deployment (#20)

--- src/TShort.Api/Data/AppDbContext.cs
+++ src/TShort.Api/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using TShort.Api.Data.Models;
 
 namespace TShort.Api.Data;
@@ -13,5 +14,16 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         ArgumentNullException.ThrowIfNull(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer();
+
+        return new AppDbContext(optionsBuilder.Options);
     }
 }
